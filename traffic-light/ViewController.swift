f3944d92401e5ed3e2a2f9ccce0624b6cbfa921b
magnosith <magnosith@icloud.com>
2023-06-14 17:27:33 -0700

    Initial Commit

---
+++ traffic-light/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  traffic-light
+//
+//  Created by Student on 14/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
